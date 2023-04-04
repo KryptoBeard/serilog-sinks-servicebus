ec7b7e324f334e60f1b74c598fc529f1be94f4c4
Dylan Reimers <dreimers@nexusinnovations.com>
2023-04-04 14:35:20 -0500

    Initial

---
+++ src/Serilog.Sinks.ServiceBus/ServiceBusConfigurationExtensions.cs
@@ -0,0 +1,53 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using Azure.Messaging.ServiceBus;
+using Serilog.Configuration;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting;
+using Serilog.Formatting.Display;
+using Serilog.Sinks.ServiceBus.Sinks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serilog.Sinks.ServiceBus
+{
+    public static class ServiceBusConfigurationExtensions
+    {
+        public static LoggerConfiguration ServiceBus(
+            this LoggerSinkConfiguration sinkConfiguration,
+            string connectionString,
+            string queueName,
+            LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
+            IFormatProvider? formatProvider = null,
+            LoggingLevelSwitch? levelSwitch = null)
+        {
+            if (sinkConfiguration is null) throw new ArgumentNullException(nameof(sinkConfiguration));
+
+            var client = new ServiceBusClient(connectionString);
+
+            return sinkConfiguration.Sink(new ServiceBusSink(client, queueName, formatProvider), restrictedToMinimumLevel, levelSwitch);
+        }
+
+        public static LoggerConfiguration ServiceBus(
+        this LoggerSinkConfiguration sinkConfiguration,
+        string nameSpace,
+        string queueName,
+        TokenCredential? tokenCredential,
+        LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
+        IFormatProvider? formatProvider = null,
+        LoggingLevelSwitch? levelSwitch = null)
+        {
+            if (sinkConfiguration is null) throw new ArgumentNullException(nameof(sinkConfiguration));
+            if(tokenCredential == null) throw new ArgumentNullException(nameof(tokenCredential));
+
+            var client = new ServiceBusClient(nameSpace, tokenCredential);
+
+            return sinkConfiguration.Sink(new ServiceBusSink(client,queueName,formatProvider),restrictedToMinimumLevel,levelSwitch);
+        }
+
+    }
+}
---
+++ src/Serilog.Sinks.ServiceBus/Sinks/ServiceBusSink.cs
@@ -0,0 +1,32 @@
+﻿using Azure.Messaging.ServiceBus;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serilog.Sinks.ServiceBus.Sinks
+{
+    public class ServiceBusSink : ILogEventSink
+    {
+        readonly ServiceBusClient _client;
+        readonly ServiceBusSender _sender;
+        readonly IFormatProvider? _formatProvider;
+
+        public ServiceBusSink(ServiceBusClient client, string queueName, IFormatProvider? formatProvider = null)
+        {
+            _formatProvider = formatProvider;
+            _client = client;
+            _sender = _client.CreateSender(queueName);
+        }
+
+        public void Emit(LogEvent logEvent)
+        {
+            var message = new ServiceBusMessage(Encoding.UTF8.GetBytes(logEvent.RenderMessage(_formatProvider)));
+            _sender.SendMessageAsync(message).GetAwaiter().GetResult();     
+        }
+    }
+}
